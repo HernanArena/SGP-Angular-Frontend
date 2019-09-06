28bc77d1100a870b49d469b4808b6cdf851095da
William Opderbeck <williamopderbeck@Williams-MBP.sar.gruposoftland.com>
2019-09-06 13:19:25 -0300

    inputs a footer

--- src/app/components/results-footer/results-footer.component.ts
+++ src/app/components/results-footer/results-footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-results-footer',
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./results-footer.component.css']
 })
 export class ResultsFooterComponent implements OnInit {
+
+
+  @Input('titulo') titulo:string="Si no encuentra la solución adecuada puede cargar un nuevo parte aquí";
+  @Input('boton1') boton1:string=" Volver ";
+  @Input('awesomeboton1') awesomeboton1:string="fa fa-arrow-left";
+  @Input('boton2') boton2:string=" Nuevo Parte ";
+  @Input('awesomeboton2') awesomeboton2:string="fa fa-pencil-square-o";
 
   constructor() { }
 
